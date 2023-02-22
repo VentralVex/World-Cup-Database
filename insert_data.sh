93c7462e06e020aad12340731f9ec3581d319fe4
VentralVex <110556715+VentralVex@users.noreply.github.com>
2023-02-22 12:00:39 -0600

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -19,40 +19,17 @@ then
   if [[ -z $WINNER_TEAM ]]
   then
     INSERT_WINNER="$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")"
-    if [[ $INSERT_WINNER == "INSERT 0 1" ]]
-    then
-      echo Inserted $WINNER successfully!
-    else 
-      echo failed to insert $WINNER!
-    fi
-  else
-    echo $WINNER already exists
   fi
 
-    OPPONENT_TEAM="$($PSQL "SELECT * FROM teams WHERE name='$OPPONENT'")"
+  OPPONENT_TEAM="$($PSQL "SELECT * FROM teams WHERE name='$OPPONENT'")"
   if [[ -z $OPPONENT_TEAM ]]
   then
     INSERT_OPPONENT="$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")"
-    if [[ $INSERT_OPPONENT == "INSERT 0 1" ]]
-    then
-      echo Inserted $OPPONENT successfully!
-    else
-      echo Failed to insert $OPPONENT!
-    fi
-  else
-    echo $OPPONENT already exists
   fi
-
 
   WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
   OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
   
   INSERT_GAME="$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")"
-  if [[ $INSERT_GAME == "INSERT 0 1" ]]
-  then
-    echo Game data inserted Successfully!
-  else
-    echo Failed to insert game data!
-  fi
 fi
 done
